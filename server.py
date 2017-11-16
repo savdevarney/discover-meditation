dd6785d9c92d734c9c5ec59f221c8d44bae7389f
savdevarney <savannah.devarney@gmail.com>
2017-11-16 01:28:46 +0000

    imported information from previous eventbrite project for reference, added API_KEY

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import os
+from pprint import pformat
+import requests
 from flask import Flask, request, render_template, session, redirect, jsonify, flash
 from eventbrite import eventbrite
 
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 OAUTH_TOKEN = os.environ["OAUTH_TOKEN"]
 ANONYMOUS_OAUTH_TOKEN = os.environ["ANONYMOUS_OAUTH_TOKEN"]
+EB_API_KEY = os.environ["EB_API_KEY"]
 
 app = Flask(__name__)
 app.secret_key = 'ABCSECRETDEF'
+
+EVENTBRITE_URL = "https://www.eventbriteapi.com/v3/"
 
 # raise errors if there are undefined variables in Jinja2
 app.jinja_env.undefined = StrictUndefined
@@ -24,3 +29,54 @@ if __name__ == "__main__":
     
     # Use the DebugToolbar
     DebugToolbarExtension(app)
+
+
+
+    # code from Hackbright lab on searching for afterparties
+
+    # query = request.args.get('query')
+    # location = request.args.get('location')
+    # distance = request.args.get('distance')
+    # measurement = request.args.get('measurement')
+    # sort = request.args.get('sort')
+
+    # # If the required information is in the request, look for afterparties
+    # if location and distance and measurement:
+
+    #     # The Eventbrite API requires the location.within value to have the
+    #     # distance measurement as well
+    #     distance = distance + measurement
+
+    #     payload = {'q': query,
+    #                'location.address': location,
+    #                'location.within': distance,
+    #                'sort_by': sort,
+    #                }
+
+    #     # For GET requests to Eventbrite's API, the token could also be sent as a
+    #     # URL parameter with the key 'token'
+    #     headers = {'Authorization': 'Bearer ' + EVENTBRITE_TOKEN}
+
+    #     response = requests.get(EVENTBRITE_URL + "events/search/",
+    #                             params=payload,
+    #                             headers=headers)
+    #     data = response.json()
+
+    #     # If the response was successful (with a status code of less than 400),
+    #     # use the list of events from the returned JSON
+    #     if response.ok:
+    #         events = data['events']
+
+    #     # If there was an error (status code between 400 and 600), use an empty list
+    #     else:
+    #         flash(":( No parties: " + data['error_description'])
+    #         events = []
+
+    #     return render_template("afterparties.html",
+    #                            data=pformat(data),
+    #                            results=events)
+
+    # # If the required info isn't in the request, redirect to the search form
+    # else:
+    #     flash("Please provide all the required information!")
+    #     return redirect("/afterparty-search")
